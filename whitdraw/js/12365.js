bdb02777199ab81b3a72b4190f2a8c3d679f8c0a
hypsongg <hypsongg@gmail.com>
2025-01-09 22:31:52 -0300

    Update 12365.js
    adjusted

--- whitdraw/js/12365.js
+++ whitdraw/js/12365.js
@@ -1,3 +1,1 @@
-if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/) && window.innerWidth > 768) {
-    window.location.href = 'https://noticia.grandprixsp2025.com/thank-you';
-  }
+
